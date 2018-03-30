a13fccf87203e5a864e2eff5023b8a920722d549
pollockcl <pollock.clee@gmail.com>
2018-03-30 10:42:41 -0600

    added functionality and tests for second user story

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
   has_many :reviews
+
+  def average_rating
+    reviews.sum(:score) / reviews.size
+  end
 end
---
+++ spec/features/book/show_page_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+context 'user visits' do
+  context 'book show page' do
+    scenario 'they see the books title and list of reviews' do
+      book = Book.create(title: 'Shrek')
+      user = User.create(name: 'Farquad')
+      review1 = Review.create(body: 'bad review', score: 1, user_id: user.id)
+      review2 = Review.create(body: 'good review', score: 5, user_id: user.id)
+      review3 = Review.create(body: 'neutral review', score: 3, user_id: user.id)
+
+      book.reviews << review1
+      book.reviews << review2
+      book.reviews << review3
+
+      visit book_path(book)
+
+      expect(page).to have_content(book.title)
+      expect(page).to have_content(review1.body)
+      expect(page).to have_content(review1.score)
+      expect(page).to have_content(review2.body)
+      expect(page).to have_content(review2.score)
+      expect(page).to have_content(review3.body)
+      expect(page).to have_content(review3.score)
+    end
+
+    scenario 'the book has an average rating' do
+      book = Book.create(title: 'Shrek')
+      user = User.create(name: 'Farquad')
+      review1 = Review.create(body: 'bad review', score: 2, user_id: user.id)
+      review2 = Review.create(body: 'good review', score: 4, user_id: user.id)
+      review3 = Review.create(body: 'neutral review', score: 3, user_id: user.id)
+
+      book.reviews << review1
+      book.reviews << review2
+      book.reviews << review3
+
+      visit book_path(book)
+
+      expect(page).to have_content(book.average_rating)
+    end
+  end
+end
